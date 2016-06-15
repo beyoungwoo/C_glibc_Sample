1cc87b3e5597c80155e9460e326b6f8bb78f3738
Beyoungwoo <beyoungwoo.geon@gmail.com>
2016-06-15 23:56:06 +0900

    -fix trade -> rent

--- get_real_estate.pl
+++ get_real_estate.pl
@@ -13,7 +13,8 @@ $ua->timeout(10);
 $ua->env_proxy;
 
 #my $url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=11440&DEAL_YMD=201512&serviceKey=';
-my $url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade';
+#my $url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade';
+my $url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent';
 
 my ($loc, $ymd);
 
@@ -50,7 +51,8 @@ if (! $response->is_success) {
 use HTML::TagParser;
 
 my $html = HTML::TagParser->new($response->decoded_content);
-my @money_list = $html->getElementsByTagName("거래금액");
+my @deposit_list = $html->getElementsByTagName("보증금액");
+my @money_list = $html->getElementsByTagName("월세금액");
 my @area_list = $html->getElementsByTagName("법정동");
 my @apt_list = $html->getElementsByTagName("아파트");
 my @size_list = $html->getElementsByTagName("전용면적");
@@ -61,7 +63,7 @@ my $result = "";
 
 for (my $i=0; $i < scalar @money_list; $i++) {
 
-
+	my $deposit = $deposit_list[$i]->innerText;
 	my $money = $money_list[$i]->innerText;
 	my $area = $area_list[$i]->innerText;
 	my $apt = $apt_list[$i]->innerText;
@@ -69,13 +71,13 @@ for (my $i=0; $i < scalar @money_list; $i++) {
 	my $month = $month_list[$i]->innerText;
 	my $day = $day_list[$i]->innerText;
 
-	$result .= sprintf ("%s, %s %s, %s (%s/%s)\n", $money, $area, $apt, $size, $month, $day);
+	$result .= sprintf ("%s(%s), %s %s, %s (%s/%s)\n", $deposit, $money, $area, $apt, $size, $month, $day);
 }
 print "$result\n";
 
 =comm
 
-0.03</전용면적><지번>446</지번><층>5</층></item><item><거래금액>    38,900</거래금액><년>2015</년><법정동>성산동</법정동><아파트>성산시영(대우)</아파트><월>12</월><일>1~10</일><전용면적>50.03</전용면적><지번>446</지번><층>6</층></item><item><거래금액>    46,000</거래금액><년>2015</년><법정동>성산동</법정동><아파트>성산시영(유원)</아파트><월>12</월><일>1~10</일><전용면적>59.43</전용면적><지번>446</지번><층>6</층></item><item><거래금액>    39,950</거래금액><년>2015</년><법정동>성산동</법정동><아파트>성산시영(선경)</아파트><월>12</월><일>1~10</일><전용면적>50.54</전용면적><지번>446</지번><층>4</층></item><item><거래금액>    42,000</거래금액><년>2015</년><법정동>성산동</법정동><아파트>월드컵아이파크(667-0)</아파트><월>12</월><일>1~10</일><전용면적>59.73</전용면적><지번>667</지번><층>17</층></item><item><거래금액>    40,300</거래금액><년>2015</년><법정동>성산동</법정동><아파트>성산시영(선경)</아파트><월>12</월><일>1~10</일><전용면적>50.54</전용면적><지번>446</지번><층>11</층></item><item><거래금액>    50,400</거래금액><년>2015</년><법정동>성산동</법정동><아파트>성산2차e-편한세상</아파트><월>12</월><일>11~20</일><전용면적>84.95</전용면적><지번>604</지번><층>12</층></item><item><거래금액>    37,800</거래금액><년>2015</년><법정동>성산동</법정동><아파트>성산시영(대우)</아파트><월>12</월><일>11~20</일><전용면적>50.03</전용면적><지번>446</지번><층>4</층></item><item><거래금액>
+><지번>1643</지번><층>15</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>    20,000</보증금액><아파트>상암월드컵파크6단지</아파트><월>12</월><월세금액>       140</월세금액><일>21~31</일><전용면적>104.32</전용면적><지번>1689</지번><층>8</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>    12,795</보증금액><아파트>상암월드컵파크9단지</아파트><월>12</월><월세금액>         0</월세금액><일>21~31</일><전용면적>59.73</전용면적><지번>1752</지번><층>12</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>    39,000</보증금액><아파트>상암월드컵파크7단지</아파트><월>12</월><월세금액>        35</월세금액><일>21~31</일><전용면적>84.45</전용면적><지번>1660</지번><층>10</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>    47,000</보증금액><아파트>상암월드컵파크7단지</아파트><월>12</월><월세금액>         0</월세금액><일>21~31</일><전용면적>84.45</전용면적><지번>1660</지번><층>4</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>     3,500</보증금액><아파트>상암월드컵파크2단지</아파트><월>12</월><월세금액>       120</월세금액><일>21~31</일><전용면적>59.92</전용면적><지번>1637</지번><층>1</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>    46,000</보증금액><아파트>상암월드컵파크10단지</아파트><월>12</월><월세금액>         0</월세금액><일>21~31</일><전용면적>84.66</전용면적><지번>1736</지번><층>9</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>    38,000</보증금액><아파트>상암월드컵파크2단지</아파트><월>12</월><월세금액>         0</월세금액><일>21~31</일><전용면적>59.92</전용면적><지번>1637</지번><층>3</층></item><item><년>2015</년><법정동>상암동</법정동><보증금액>    50,000</보증금액><아파트>상암월드컵파크4단지</아파트><월>12</월><월세금액>         0</월세금액><일>21~31</일><전용면적>84.89</전용면적><지번>1655</지번><층>18</층></item></items></body></response>
 
     my $html = HTML::TagParser->new( "http://www.kawa.net/xp/index-e.html" );
     my @list = $html->getElementsByTagName( "a" );
