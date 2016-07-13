9ad4fd79d5f775e1a1bad92fce5ccec3d27b26a3
Beyoungwoo <beyoungwoo.geon@gmail.com>
2016-07-13 13:23:38 +0900

    fix web browser display message

--- src/com/jy1/controller/ControllerMain.java
+++ src/com/jy1/controller/ControllerMain.java
@@ -16,7 +16,8 @@ public class ControllerMain {
 	public ModelAndView helloWorld() {
  
 		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+				+ "<h3> Controller, RequestMapping 구조 참고 </h3>"
+				+ "This message is coming from src/com/jy1/controller/ControllerMain.java </div><br><br>";
 		return new ModelAndView("welcome", "message", message);
 	}
 	
@@ -24,7 +25,7 @@ public class ControllerMain {
 	public ModelAndView linuxResource() {
  
 		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+				+ "<h3>resource_check message</h3></div><br><br>";
 		return new ModelAndView("resource_check", "message", message);
 	}
 
