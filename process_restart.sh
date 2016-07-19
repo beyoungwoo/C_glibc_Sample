49850050dd87eae4acad02b9f43bf4d801c3da59
Beyoungwoo <beyoungwoo.geon@gmail.com>
2016-07-19 09:58:47 +0900

    - fix restart Perl script file name

--- process_restart.sh
+++ process_restart.sh
@@ -6,15 +6,15 @@
 # crontab에 등록하여 주기적으로 실행시킴
 # 
 
-case "$(pidof perl /home/beyoungwoo/src/Perl/check_it_out.pl | wc -w)" in
+case "$(pidof perl /home/beyoungwoo/src/Perl/real_estate_rent.pl | wc -w)" in
 
 0)  echo "Restarting Telegram bot:     $(date)" >> /home/beyoungwoo/src/BASH/restart_log
-    nohup /home/beyoungwoo/src/Perl/check_it_out.pl
+    nohup /home/beyoungwoo/src/Perl/real_estate_rent.pl
     ;;
 1)  # all ok
     ;;
 *)  echo "Removed double Telegram bot: $(date)" >> /home/beyoungwoo/src/BASH/restart_log
-    kill $(pidof perl /home/beyoungwoo/src/Perl/check_it_out.pl | awk '{print $1}')
+    kill $(pidof perl /home/beyoungwoo/src/Perl/real_estate_rent.pl | awk '{print $1}')
     ;;
 esac
 
