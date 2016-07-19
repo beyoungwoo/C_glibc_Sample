3afa1ca36c2efce158004366f89e99e03c485e1d
Y1 <beyoungwoo.geon@gmail.com>
2016-07-19 09:54:49 +0900

    Update process_restart.sh

--- process_restart.sh
+++ process_restart.sh
@@ -3,8 +3,10 @@
 # * Reference
 # http://stackoverflow.com/questions/20162678/linux-script-to-check-if-process-is-running-act-on-the-result
 #
+# crontab에 등록하여 주기적으로 실행시킴
+# 
 
-case "$(pidof perl ./check_it_out.pl | wc -w)" in
+case "$(pidof perl /home/beyoungwoo/src/Perl/check_it_out.pl | wc -w)" in
 
 0)  echo "Restarting Telegram bot:     $(date)" >> /home/beyoungwoo/src/BASH/restart_log
     nohup /home/beyoungwoo/src/Perl/check_it_out.pl
@@ -12,7 +14,7 @@ case "$(pidof perl ./check_it_out.pl | wc -w)" in
 1)  # all ok
     ;;
 *)  echo "Removed double Telegram bot: $(date)" >> /home/beyoungwoo/src/BASH/restart_log
-    kill $(pidof perl ./check_it_out.pl | awk '{print $1}')
+    kill $(pidof perl /home/beyoungwoo/src/Perl/check_it_out.pl | awk '{print $1}')
     ;;
 esac
 
