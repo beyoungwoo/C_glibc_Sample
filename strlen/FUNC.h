03edb2142e1e6520836f482a67e787adb90455e6
Beyoungwoo <beyoungwoo.geon@gmail.com>
2016-09-30 13:55:42 +0900

    Move file

--- strlen/FUNC.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-size_t strlen (const char *str);
--- strstr/FUNC.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-char* strrstr(const char *haystack, const char *needle);
