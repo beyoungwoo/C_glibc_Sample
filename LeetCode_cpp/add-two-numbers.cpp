c93d9c8228411ca31b712b324104ea5d8957325e
Beyoungwoo <beyoungwoo.geon@gmail.com>
2016-10-05 13:40:42 +0900

    Add solution 2

--- LeetCode_cpp/add-two-numbers.cpp
+++ LeetCode_cpp/add-two-numbers.cpp
@@ -7,8 +7,47 @@
  * };
  */
 class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-    }
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            if (l1 == NULL || l2 == NULL) {
+                return NULL;
+            }
+
+            int carry = 0;
+            ListNode* output = new ListNode(0);
+            ListNode* cur = output;
+
+            while (true) {
+
+                if (l1 != NULL) {
+                    carry += l1->val;
+                    l1 = l1->next;
+                }
+
+                if (l2 != NULL) {
+                    carry += l2->val;
+                    l2 = l2->next;
+                }
+
+                cur->val = carry % 10;
+                carry /= 10;
+
+                /* 'carry != 0' check for this exception
+                 * 
+                 *    [1,2,3,4,5]
+                 * +  [9,8,8,8,8]
+                 * ------------------
+                 *    [0,1,2,3,4,1]
+                 *               _
+                 */
+                if (l1 != NULL || l2 != NULL || carry != 0) {
+                    cur = (cur->next = new ListNode(0));
+                }
+                else {
+                    break;
+                }
+            }
+
+            return output;
+        }
 };
