48c357cb777608e853eb696fd195922c2e153758
byung-u <iam.byungwoo@gmail.com>
2016-12-02 13:16:42 +0900

    Fix file

--- C/temp.c
+++ C/temp.c
@@ -1,2 +1,24 @@
+#include <stdarg.h>
 
-asdfsdflkj
+int sum(int nargs, ...)
+{
+    va_list argptr;
+    int i, total = 0;
+
+    va_start(argptr, nargs);
+    for (i = 0; i < nargs, i++) {
+        total += va_arg(arg_ptr, int);
+    }
+    va_end(argptr);
+    return total;
+}
+
+int main()
+{
+    int s1, s2;
+
+    s1 = sum(5, 1,2 , 3, 4, 5);
+    printf("%d\n", s1);
+
+    return 0;
+}
